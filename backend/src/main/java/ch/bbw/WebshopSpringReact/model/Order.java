217292dfbf345c728f5adeb2e5802f497092797f
Adrian Büchi <adrianbuechi@gmail.com>
2019-03-04 11:22:53 +0100

    Fixed startup

--- backend/src/main/java/ch/bbw/WebshopSpringReact/model/Order.java
+++ backend/src/main/java/ch/bbw/WebshopSpringReact/model/Order.java
@@ -2,9 +2,7 @@ package ch.bbw.WebshopSpringReact.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
 import javax.validation.Valid;
