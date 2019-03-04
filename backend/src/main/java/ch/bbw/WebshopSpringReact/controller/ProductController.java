3474e116c743de56fdba839c1ca4671e0ac9e161
Adrian Büchi <adrianbuechi@gmail.com>
2019-03-04 10:53:38 +0100

    moved module

--- backend/src/main/java/ch/bbw/WebshopSpringReact/controller/ProductController.java
+++ backend/src/main/java/ch/bbw/WebshopSpringReact/controller/ProductController.java
@@ -2,10 +2,11 @@ package ch.bbw.WebshopSpringReact.controller;
 
 import ch.bbw.WebshopSpringReact.model.Product;
 import ch.bbw.WebshopSpringReact.service.ProductService;
-import com.sun.istack.internal.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/api/products")
